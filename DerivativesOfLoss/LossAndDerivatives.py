65336a12db483c4edcf5797acf1879acce4bc881
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2023-12-25 16:51:39 +0300

    Create LossAndDerivatives.py

---
+++ DerivativesOfLoss/LossAndDerivatives.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class LossAndDerivatives:
+    @staticmethod
+    def mse(X, Y, w):
+        """
+        X : numpy array of shape (`n_observations`, `n_features`)
+        Y : numpy array of shape (`n_observations`, `target_dimentionality`) or (`n_observations`,)
+        w : numpy array of shape (`n_features`, `target_dimentionality`) or (`n_features`,)
+
+        Return : float
+            single number with MSE value of linear model (X.dot(w)) with no bias term
+            on the selected dataset.
+
+        Comment: If Y is two-dimentional, average the error over both dimensions.
+        """
+
+        return np.mean((X.dot(w) - Y) ** 2)
+      
