d985006d7a88f7cc5cc5f1dd93948bde35deee1e
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2023-12-25 11:59:02 +0300

    Create PowerMethod.py

---
+++ PowerMethod/PowerMethod.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def get_dominant_eigenvalue_and_eigenvector(data, num_steps):
+    """
+    data: np.ndarray – symmetric diagonalizable real-valued matrix
+    num_steps: int – number of power method steps
+
+    Returns:
+    eigenvalue: float – dominant eigenvalue estimation after `num_steps` steps
+    eigenvector: np.ndarray – corresponding eigenvector estimation
+    """
+
+    # Initialize a random vector of the same size as the matrix
+    n = data.shape[0]
+    eigenvector = np.random.rand(n)
