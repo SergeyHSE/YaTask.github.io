26bd0bf3db63f5406fbaae7efad09343a0a67c0f
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2023-12-25 16:56:20 +0300

    Update LossAndDerivatives.py

--- DerivativesOfLoss/LossAndDerivatives.py
+++ DerivativesOfLoss/LossAndDerivatives.py
@@ -17,3 +17,22 @@ class LossAndDerivatives:
 
         return np.mean((X.dot(w) - Y) ** 2)
       
+    @staticmethod
+    def mae(X, Y, w):
+        """
+        X : numpy array of shape (`n_observations`, `n_features`)
+        Y : numpy array of shape (`n_observations`, `target_dimentionality`) or (`n_observations`,)
+        w : numpy array of shape (`n_features`, `target_dimentionality`) or (`n_features`,)
+
+        Return: float
+            single number with MAE value of linear model (X.dot(w)) with no bias term
+            on the selected dataset.
+
+        Comment: If Y is two-dimentional, average the error over both dimensions.
+        """
+        error = X.dot(w) - Y
+        if len(error.shape) > 1:  # Check if Y is two-dimensional
+            return np.mean(np.sum(np.abs(error), axis=0))  # Average over both dimensions of Y
+        else:
+            return np.mean(np.abs(error))
+
