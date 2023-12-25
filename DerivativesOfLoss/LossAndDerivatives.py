13a605708efb1999c4cf1fff99e77a7e1091a65c
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2023-12-25 16:59:55 +0300

    Update LossAndDerivatives.py

--- DerivativesOfLoss/LossAndDerivatives.py
+++ DerivativesOfLoss/LossAndDerivatives.py
@@ -60,3 +60,25 @@ class LossAndDerivatives:
         """
         return np.sum(np.abs(w))
 
+    @staticmethod
+    def mse_derivative(X, Y, w):
+        """
+        X : numpy array of shape (`n_observations`, `n_features`)
+        Y : numpy array of shape (`n_observations`, `target_dimentionality`) or (`n_observations`,)
+        w : numpy array of shape (`n_features`, `target_dimentionality`) or (`n_features`,)
+
+        Return : numpy array of same shape as `w`
+
+        Computes the MSE derivative for linear regression (X.dot(w)) with no bias term
+        w.r.t. w weight matrix.
+
+        Please mention, that in case `target_dimentionality` > 1 the error is averaged along this
+        dimension as well, so you need to consider that fact in derivative implementation.
+        """
+        n_observations = X.shape[0]
+        error = X.dot(w) - Y
+        if len(error.shape) > 1:
+          # Если `target_dimentionality` > 1, усредняем ошибку по этой размерности.
+            return (2 / n_observations) * X.T.dot(error) / error.shape[1]
+        else:
+            return (2 / n_observations) * X.T.dot(error)
