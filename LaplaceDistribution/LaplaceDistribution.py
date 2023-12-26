79e22e96ca29ed5ee140770f31e19c7695527bc1
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2023-12-26 11:38:40 +0300

    Update LaplaceDistribution.py

--- LaplaceDistribution/LaplaceDistribution.py
+++ LaplaceDistribution/LaplaceDistribution.py
@@ -25,3 +25,16 @@ class LaplaceDistribution:
         self.scale = np.mean(np.abs(features - self.loc), axis=0)
         ####
 
+    def logpdf(self, values):
+        '''
+        Returns logarithm of probability density at every input value.
+        Args:
+            values: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
+        '''
+        ####
+        # Do not change the class outside of this block
+        log_prob = -np.log(2 * self.scale) - np.abs(values - self.loc) / self.scale
+        
+        return log_prob
+        ####
+        
