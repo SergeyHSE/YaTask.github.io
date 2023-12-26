283b14bd4107ff0839fe960a3a71905de5b40844
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2023-12-26 11:36:00 +0300

    Create LaplaceDistribution.py

---
+++ LaplaceDistribution/LaplaceDistribution.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class LaplaceDistribution:    
+    @staticmethod
+    def mean_abs_deviation_from_median(x: np.ndarray):
+        '''
+        Args:
+        - x: A numpy array of shape (n_objects, n_features) containing the data
+          consisting of num_train samples each of dimension D.
+        '''
+        ####
+        # Do not change the class outside of this block
+        median = np.median(x, axis=0)
+        mad = np.mean(np.abs(x - median), axis=0)
+        return mad
+        ####
