8a1736ca30a4be38527fff50dfbc1b225fa258bf
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2023-12-27 13:28:30 +0300

    Update k_nearest_neighbor.py

--- KNearestNeighbor/k_nearest_neighbor.py
+++ KNearestNeighbor/k_nearest_neighbor.py
@@ -19,3 +19,30 @@ class KNearestNeighbor:
         """
         self.X_train = X
         self.y_train = y
+        
+    def predict(self, X, k=1, num_loops=0):
+        """
+        Predict labels for test data using this classifier.
+
+        Inputs:
+        - X: A numpy array of shape (num_test, D) containing test data consisting
+             of num_test samples each of dimension D.
+        - k: The number of nearest neighbors that vote for the predicted labels.
+        - num_loops: Determines which implementation to use to compute distances
+          between training points and testing points.
+
+        Returns:
+        - y: A numpy array of shape (num_test,) containing predicted labels for the
+          test data, where y[i] is the predicted label for the test point X[i].
+        """
+        if num_loops == 0:
+            dists = self.compute_distances_no_loops(X)
+        elif num_loops == 1:
+            dists = self.compute_distances_one_loop(X)
+        elif num_loops == 2:
+            dists = self.compute_distances_two_loops(X)
+        else:
+            raise ValueError('Invalid value %d for num_loops' % num_loops)
+
+        return self.predict_labels(dists, k=k)
+
