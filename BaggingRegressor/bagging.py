305688a9a14dcaa417bf24c225c5b6efc9458fc4
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2023-12-28 11:09:21 +0300

    Update bagging.py

--- BaggingRegressor/bagging.py
+++ BaggingRegressor/bagging.py
@@ -10,3 +10,14 @@ class SimplifiedBaggingRegressor:
         self.target = None
         self.list_of_predictions_lists = None
         self.oob_predictions = None
+
+    def _generate_splits(self, data: np.ndarray):
+        '''
+        Generate indices for every bag and store in self.indices_list list
+        '''
+        self.indices_list = []
+        data_length = len(data)
+        for bag in range(self.num_bags):
+            # Your Code Here
+            indices = np.random.choice(data_length, data_length, replace=True)
+            self.indices_list.append(indices)
